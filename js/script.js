5ed4ff6edc65039c15cb44365f495d3af55fae38
Will <willian_aud521@hotmail.com>
2021-02-27 00:21:33 -0300

    carousel att hover effect

--- js/script.js
+++ js/script.js
@@ -1,81 +1,9 @@
 $(document).ready(function(){
     
-    let slider = $('#lightSlider').lightSlider({
-        controls: false,
-        prevHtml: '<i class="fas fa-chevron-left"></i>',
-        nextHtml: '<i class="fas fa-chevron-right"></i>',
-        slideMargin: 1,
-        responsive: [
+    carrosel1 = new InsCarousel('owl-carousel1').instacia()
 
-            {
-                breakpoint: 2000,
-                settings: {
-                    item: 7.61,
-                    slideMove: 5,
-                    controls: true,
-                }
-            },
+    carrosel2 = new InsCarousel('owl-carousel2').instacia()
 
-            {
-                breakpoint: 1700,
-                settings: {
-                    item: 6.53,
-                    slideMove: 4,
-                    controls: true
-                }
-            },
-
-            {
-                breakpoint: 1400,
-                settings: {
-                    item: 5.44,
-                    slideMove: 3,
-                    controls: true,
-                    
-                }
-            },
-
-            {
-                breakpoint: 1100,
-                settings: {
-                    item: 4.35,
-                    slideMove: 3,
-                    controls: true
-                }
-            },
-
-            {
-                breakpoint: 1024,
-                settings: {
-                    item: 4.18,
-                    slideMove: 3,
-                    controls: false
-                }
-            },
-
-            {
-                breakpoint: 800,
-                settings: {
-                    item: 3.14,
-                    slideMove: 3,
-                    controls: false
-                }
-            },
-
-            {
-                breakpoint: 500,
-                settings: {
-                    item: 2.09,
-                    slideMove: 2,
-                    controls: false
-                }
-            },
-
-        ]
-
-    })
-    
-    console.log(slider.getCurrentSlideCount())
 
     $('.hamburguer').click( ()=>{
         if( $('nav').css('visibility') == 'visible'){
@@ -96,33 +24,135 @@ $(document).ready(function(){
 
     })
 
-    gerarImagens()
 })
 
 
+class InsCarousel{
+    constructor(nomeClasse){
+        this.nomeClasse =  "." + nomeClasse
+    }
+
+    instacia(){
+        let carrosel2 = new Carrosel(this.nomeClasse)
+        carrosel2.conteudo_carrosel()
+        carrosel2.owl_carousel()
+    }
+    
+}
+
+class Carrosel{
+    
+    constructor(nomeClasse){
+        this.nomeClasse =  nomeClasse
+    }
+
+
+    conteudo_carrosel(){
+
+        for(let i = 1; i<8; i++){
+
+            //* LI
+            let li = document.createElement('li')
+            li.className = 'item'
+            
+            //* IMG
+            let img = document.createElement('img')
+            $(img).attr({
+                src: `../img/mini${i}.png`
+            })
+
+            let div = document.createElement('div')
+            div.className = 'info-filme'
+
+            //*ARVORE
+            li.prepend(img)
+            li.append(div)
+            document.querySelector(this.nomeClasse).append(li)
+        }
+
+        let li = document.createElement('li')
+        document.querySelector(this.nomeClasse).append(li)
+    }
+
+
+    owl_carousel(){
+
+        $(this.nomeClasse).owlCarousel({
+
+            navText: [
+                '<i class="fas fa-chevron-left"></i>',
+                '<i class="fas fa-chevron-right"></i>'
+            ],
+
+            margin: 5,
+            touchDrag: false,
+            nav: false,
+            dots: false,
+            loop: true,
+            responsive: {
+                1700:{
+                    items: 6.56,
+                    smartSpeed: 100,
+                    slideBy: 6,
+                }
+            }
+        })
+    }
+
+    
+}
+
+
+function carousel(id){
+    $(id).owlCarousel({
+
+        navText: [
+            '<i class="fas fa-chevron-left"></i>',
+            '<i class="fas fa-chevron-right"></i>'
+        ],
+        margin: 5,
+        touchDrag: false,
+        nav: false,
+        dots: false,
+        loop: true,
+        responsive: {
+            1700:{
+                items: 6.5,
+                smartSpeed: 100,
+                singleItem: false,
+                slideBy: 6,
+            }
+        }
+    })
+}
 
 // * GERADOR DE 'LI' COM IMAGENS
 function gerarImagens(){
     //* CRIANDO LI E IMAGENS
     
-    for(let j = 0; j<3; j++){
 
-        for(let i = 1; i<8; i++){
-            let li = document.createElement('li')
-            li.className = 'li-img'
+    for(let i = 1; i<8; i++){
+
+        //* LI
+        let li = document.createElement('li')
+        li.className = 'item'
         
-            let img = document.createElement('img')
+        //* IMG
+        let img = document.createElement('img')
+        $(img).attr({
+            src: `../img/mini${i}.png`
+        })
 
-            $(img).attr({
-                src: `../img/mini${i}.png` 
-            })
+        let div = document.createElement('div')
+        div.className = 'info-filme'
 
-            li.prepend(img)
-            
-            document.getElementById('lightSlider').append(li)
-        }
+        //*ARVORE
+        li.prepend(img)
+        li.append(div)
+        document.getElementById('owl-carousel1').append(li)
     }
 
     let li = document.createElement('li')
-    document.getElementById('lightSlider').append(li)
+    document.getElementById('owl-carousel1').append(li)
+
 }
