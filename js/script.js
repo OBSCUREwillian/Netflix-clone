ec111df958f5648d254be8eae7c41848c5ea2210
Will <willian_aud521@hotmail.com>
2021-03-10 23:47:55 -0300

    carousel efeito hover finalizado

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     
-    let carrosel1 = new InsCarousel('owl-carousel1').instacia()
-    let carrosel2 = new InsCarousel('owl-carousel2').instacia()
-    let carrosel3 = new InsCarousel('owl-carousel3').instacia()
+    let carrosel1 = new InsCarousel('owl-carousel1').instancia()
+    let carrosel2 = new InsCarousel('owl-carousel2').instancia()
+    let carrosel3 = new InsCarousel('owl-carousel3').instancia()
 
 
     $('.hamburguer').click( ()=>{
@@ -26,13 +26,13 @@ $(document).ready(function(){
 
 })
 
-
+//* Classe que instacia a classe Carrosel
 class InsCarousel{
     constructor(nomeClasse){
         this.nomeClasse =  "." + nomeClasse
     }
 
-    instacia(){
+    instancia(){
         let carrosel = new Carrosel(this.nomeClasse)
         carrosel.conteudo_carrosel()
         carrosel.owl_carousel()
@@ -43,10 +43,12 @@ class Carrosel{
     
     constructor(nomeClasse){
         this.nomeClasse =  nomeClasse
+
     }
 
 
     conteudo_carrosel(){
+
         for(let j = 0; j<2; j++){
             for(let i = 1; i<8; i++){
 
@@ -69,74 +71,123 @@ class Carrosel{
                 document.querySelector(this.nomeClasse).append(li)
             }
         }
-        
 
+        
        
     }
 
     owl_carousel(){
 
-        $(this.nomeClasse).owlCarousel({
+        let navPrevClone
+        let owl = $(this.nomeClasse).owlCarousel({
             
             onInitialized: (e)=>{
-                //* Desabilitando o botão prev 
-                    $('.owl-carousel .owl-nav button.owl-prev').css(
-                        {"background-color": "rgb(20, 20, 20)"}
-                    )
-                    $('.owl-prev').prop('disabled', true)
-                    $('.owl-prev i').css('color', 'rgb(20, 20, 20)')
+
+                //* Nav Original (NEXT)
+                    
+                    //* Nav Original - aqui se encontra a navegação original mas escondendo o botão prev(display - none) para que só fique aparecendo o botão next - Alem de jogar a nav Original toda para a direita
+                    let nav = $(e.currentTarget).find('.owl-nav')
+                    $(nav).css('right', '0')
+                    let prev = $(nav).find('.owl-prev')
+                    prev.css('display', 'none')
+
+                //*--------------------------------------------------|
+
+
+
+                //* Nav Copia (PREV)
+
+                    //* Nav Clone - Aqui é o mesmo processo visto acima - só que criando um clone da Nav Original e escondendo o botão next - Assim sobrando o botão prev, pois como vinmos acima o botão next ficou a cargo da Nav original - Alem de jogar esse clone totalmente para a esquerda
+                    let navClone = $(nav).clone()
+                    navClone.css('left', '0')
+                    navClone.appendTo(e.currentTarget)
+                    $(navClone).find('.owl-next').css('display', 'none')
+
+                    let prevClone = $(navClone).find('.owl-prev')
+                    prevClone.css('display', 'block')
+                    $(prevClone).addClass('prev-black')
+                    $(prevClone).prop("disabled", true)
+
+                    // * Funcao subtitui o clique do botão prev da Nav Original para o botão prev da Nav Clone
+                    $(prevClone).on('click', function(){
+                        prev.click()
+                    })
+
+                    navPrevClone = prevClone
                 //*--------------------------------------------------|
 
                 
-                //* Arrumando barra de nav sobrepondo o efeito hover do item(filme/serie)
-                    $('.owl-item.active').hover(
-                        // e=>{
-                        //     $('div.owl-nav').css('z-index', '-1')
-                        //     $('div.owl-stage-outer, .owl-stage, .owl-carousel').css('z-index', '2')
-                        // },
-                        // e=>{
-                        //     $('div.owl-nav').css('z-index', '3')
-                            
-                        // }
-                    )
                 
-                    // div.owl-nav - DIMINUIR - ZINDEX
-                // div.owl-stage-outer, .owl-stage, .owl-carousel - AUMENTAR ZINDEX
-                
+                //* Movendo os items-active(primeiro e ultimo) do carrosel para que não colidão com os botões prev e next
+                    // let itens = $(e.currentTarget).find('.owl-item.active')
+                    // let primItem = itens[0]
+                    // let ultmItem = itens[5]
+                    // $(primItem).on('mouseenter', function(e){
+
+                    //     $(e.currentTarget).css( 'left', '70px')
+
+                    // })
+                    // $(primItem).hover(
+                    //     e=>{
+                    //     },
+                    //     e=>{
+                    //         $(e.currentTarget).css('left', '0px')
+
+
+
+                    //     }
+                    // )
+                //*--------------------------------------------------|
+
+                 
             },
 
-            onTranslate: e =>{
-                //* Habilitando o botão prev
-                    let prev = e.currentTarget.children[1].children[0] 
-                    if($(prev).css('background-color') == 'rgb(20, 20, 20)'){
+            
 
-                        $(prev).prop('disabled', false)
-                        $(prev).addClass('btn-prev')
-                        $(prev.children[0]).css('color', '#fff')
+            onTranslate: e =>{
+                
+                // //* Habilitando o botão prev
+                    $(navPrevClone).removeClass('prev-black')
+                    if($(navPrevClone).prop("disabled")){
+                        $(navPrevClone).prop("disabled", false)
                     }
+
                 //*-----------------------------------------------------------|
-        
+
+                   
+                
             },
 
             navText: [
                 '<i class="fas fa-chevron-left"></i>',
                 '<i class="fas fa-chevron-right"></i>'
             ],
-
-
+            stagePadding: 80,
             margin: 5,
             mouseDrag: false,
             nav: true,
             dots: false,
             loop: true,
             responsive: {
+                800:{
+                    items: 3.54,
+                },
+                1100:{
+                    items: 4.54,
+                },
+                1400:{
+                    items: 5.54,
+                },
                 1700:{
-                    items: 6.54,
+                    items: 6.001,
                     smartSpeed: 100,
                     slideBy: 6
-                }
+                },
+                
             }
         })
+
+
     }
     
 }
